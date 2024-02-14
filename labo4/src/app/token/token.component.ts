6c1e0ea9c17b47541091bfab329b671d6f696efe
n <2185802@cegepmontpetit.ca>
2024-02-14 11:17:02 -0500

    Import

---
+++ labo4/src/app/token/token.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Artist } from '../models/artist';
+import { SpotifyService } from '../services/spotify.service';
+
+@Component({
+  selector: 'app-token',
+  templateUrl: './token.component.html',
+  styleUrls: ['./token.component.css']
+})
+export class TokenComponent implements OnInit {
+
+  artistName : string = "";
+  artist ?: Artist;
+  spotifyToken ?: string;
+
+  constructor(public spotify : SpotifyService) { }
+
+  ngOnInit() {
+    // Allo 👋
+    this.spotify.connect();
+  }
+
+  async getArtist() : Promise<void>{
+    // Allo 👋
+    this.artist = await this.spotify.searchArtist(this.artistName);
+  }
+
+}
